b76b0d3ad3547b82e4d51836b766e2e22315b34c
Andre Boothe <boothe.andre@live.com>
2022-11-11 09:58:03 -0500

    test

--- intro/edge-of-the-ocean/adjacentElementsProduct.js
+++ intro/edge-of-the-ocean/adjacentElementsProduct.js
@@ -2,7 +2,7 @@
 Given an array of integers,
 find the pair of adjacent elements that has the largest product and return that product.
 
-Example
+Example.
 
 For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 adjacentElementsProduct(inputArray) = 21.
@@ -11,18 +11,16 @@ adjacentElementsProduct(inputArray) = 21.
 */
 
 function adjacentElementsProduct(inputArray) {
-
-    var product = 0;
-    for(var i = 0; i < inputArray.length - 1;i++){
-        var newProduct = inputArray[i] * inputArray[i+1];
-        if( i != 0){
-
-           if(product < newProduct){
-              product = newProduct;
-              }
-        }else{product = newProduct;}
-
-
+  var product = 0;
+  for (var i = 0; i < inputArray.length - 1; i++) {
+    var newProduct = inputArray[i] * inputArray[i + 1];
+    if (i != 0) {
+      if (product < newProduct) {
+        product = newProduct;
+      }
+    } else {
+      product = newProduct;
     }
-    return product;
+  }
+  return product;
 }
